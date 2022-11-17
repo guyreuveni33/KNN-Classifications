1958a0aba227e3870ec08bee0b54d2f2eb31f3c0
guyreuveni33 <guyhaim55@gmail.com>
2022-11-17 23:38:24 +0200

    this is the un working version of the main with regex

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <regex>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<double> FirstVector, SecondVector;
+    std::string TextLineOne;
+    std::string TextLineTwo;
+    getline(std::cin, TextLineOne);
+    double num;
+    std::istringstream LineStream(TextLineOne);
+    std::regex rx("[+-]?([0-9]*[\\.])?[0-9]+|[“ “]");
+    while (!std::regex_search(TextLineOne, rx)) {
+        std::cout << "error\n";
+        LineStream.clear();
+        getline(std::cin, TextLineOne);
+        LineStream.str(TextLineOne);
+        std::istringstream LineStream(TextLineOne);
+    }
+    while (LineStream >> num) {
+        FirstVector.push_back(num);
+    }
+    LineStream.clear();
+    getline(std::cin, TextLineTwo);
+    LineStream.str(TextLineTwo);
+    while (LineStream >> num) {
+        SecondVector.push_back(num);
+    }
+    for (int i = 0; i < FirstVector.size(); i++)
+        std::cout << FirstVector.at(i) << ' ';
+
+    for (int i = 0; i < SecondVector.size(); i++)
+        std::cout << SecondVector.at(i) << ' ';
+
     return 0;
+
+
 }
+
