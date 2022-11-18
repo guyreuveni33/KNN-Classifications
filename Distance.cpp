4d89638cd61b7e35623886413dfa301bdf11ca28
nivswisa11 <swisaniv@gmail.com>
2022-11-18 02:49:01 +0200

    creating a distance source file and header file

--- Distance.cpp
+++ Distance.cpp
@@ -1,5 +1,24 @@
-//
-// Created by niv swisa on 18/11/2022.
-//
-
 #include "Distance.h"
+#include <vector>
+#include <cmath>
+
+double Minkowski (std::vector<double> &v1, std::vector<double> &v2, int x) {
+    int i;
+    double sum = 0;
+    for (i = 0; i < v1.size(); i++) {
+        double temp = v1.at(i) - v2.at(i);
+        if (temp < 0) {
+            temp = temp * (-1);
+        }
+        sum += pow(temp, x);
+    }
+    double distance = pow(sum, 1/x);
+    return distance;
+}
+
+double Euclidean (std::vector<double> &v1, std::vector<double> &v2) {
+    return Minkowski(v1, v2, 2);
+}
+double Manhattan (std::vector<double> &v1, std::vector<double> &v2) {
+    return Minkowski(v1, v2, 1);
+}
