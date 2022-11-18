78bb72e173418d6781ea19f90d1313309c8d4592
nivswisa11 <swisaniv@gmail.com>
2022-11-18 02:01:53 +0200

    creating a distance source file and header file

---
+++ Distance.cpp
@@ -0,0 +1,5 @@
+//
+// Created by niv swisa on 18/11/2022.
+//
+
+#include "Distance.h"
---
+++ Distance.h
@@ -0,0 +1,8 @@
+//
+// Created by niv swisa on 18/11/2022.
+//
+
+#ifndef AP2022_DISTANCE_H
+#define AP2022_DISTANCE_H
+
+#endif //AP2022_DISTANCE_H
