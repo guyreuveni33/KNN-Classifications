3d5bffa41abc926ba9cc9f93042aba968a7bbb80
guyreuveni33 <guyhaim55@gmail.com>
2022-11-18 02:47:35 +0200

    this is the Canberra and Chebyshev algorithms

--- Distance.cpp
+++ Distance.cpp
@@ -1,5 +1,45 @@
-//
-// Created by niv swisa on 18/11/2022.
-//
+#include <vector>
+#include <valarray>
 
-#include "Distance.h"
+double Canberra(std::vector<double> &v1, std::vector<double> &v2) {
+    int l;
+    double sum = 0;
+    l = v1.size();
+    for (int i = 0; i < l; ++i) {
+        if ((v1.at(i) < 0)) {
+            v1.push_back(v1.at(i) * (-1));
+        }
+        if ((v2.at(i) < 0)) {
+            v2.push_back(v2.at(i) * (-1));
+            if (v1.at(i) == 0 && v2.at(i) == 0) {
+                continue;
+            }
+            sum = v1.at(i) - v2.at(i) / (v1.at(i)) + (v2.at(i));
+        }
+        return sum;
+    }
+}
+
+double Chebyshev(std::vector<double> &v1, std::vector<double> &v2) {
+    int l;
+    double max;
+    l = v1.size();
+    if ((v1.at(0) < 0)) {
+        v1.push_back(v1.at(0) * (-1));
+    }
+    if ((v2.at(0) < 0)) {
+        v2.push_back(v2.at(0) * (-1));
+        max = v1.at(0) - v2.at(0);
+        for (int i = 1; i < l; ++i) {
+            if ((v1.at(i) < 0)) {
+                v1.push_back(v1.at(i) * (-1));
+            }
+            if ((v2.at(i) < 0)) {
+                v2.push_back(v2.at(i) * (-1));
+            }
+            if (v1.at(0) - v2.at(0) > max)
+                max = (v1.at(0) - v2.at(0));
+        }
+        return max;
+    }
+}
