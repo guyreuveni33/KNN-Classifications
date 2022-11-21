43eadbab1b749d770956b4b4d716f3f0fbc65568
guyreuveni33 <guyhaim55@gmail.com>
2022-11-21 22:11:36 +0200

    this is the 1.5 version of our assignment. Guy&Niv.

--- main.cpp
+++ main.cpp
@@ -17,25 +17,33 @@ int PrecisionNum(double d) {
         return 1;
     } else return 16;
 }
+int ValueCheck(string TextLine){
 
+}
 int numCheck(string s) {
     int i, flag = 0;
-    if (!isdigit(s[0])) {
+    if (!isdigit(s.at(0))) {
         return 0;
     }
-    for (i = 1; i < s.size(); i++) {
-        if (!(isdigit(s[i]))) {
-            if (s[i] == 46) {
+    for ( i = 0; i < s.size(); i++) {
+        if (!(isdigit(s.at(i)))) {
+            if (s.at(i) == 46) {
+                if (i == s.size()-1) {
+                    return 0;
+                }
                 flag++;
                 continue;
             }
+            return 0;
         }
-        if (flag>=2) {
+        if (flag >= 2) {
             return 0;
         }
     }
-    return 1;
+return 1;
 }
+
+
 
 /**
  * It takes two vectors of doubles, and returns the Euclidean distance between them
@@ -48,8 +56,21 @@ int main() {
     std::string TextLineTwo;
     getline(std::cin, TextLineOne);
     int i, k;
+    if (TextLineOne.empty()){
+        std::cout << "N";
+        return 0;
+
+    }
+    if (TextLineOne.at(0)>=1 && TextLineOne.at(0)<=32) {
+        std::cout << "0E";
+        return 0;
+    }
     for (i = 1; i < TextLineOne.size(); i++) {
-        if (TextLineOne[i - 1] == 32 && TextLineOne[i] == 32) {
+        if (TextLineOne.at(i)>=1 && TextLineOne.at(i)<=31) {
+            std::cout << "Werror";
+            return 0;
+        }
+        if (TextLineOne.at(i-1) == 32 && TextLineOne.at(i) == 32) {
             std::cout << "error";
             return 0;
         }
@@ -63,26 +84,12 @@ int main() {
             return 0;
         }
     }
-    /**
-    std::regex rx("(([-]?([0-9]+[\\.])?([0-9]+))([“ “])?)(([“ “][-]?([0-9]+[\\.])?([0-9]+))([“ “])?)*");
-    while (!std::regex_match(TextLineOne, rx)) {
-        std::cout << "error\n";
-        LineStream.clear();
-        getline(std::cin, TextLineOne);
-        LineStream.str(TextLineOne);
-    }
     while (LineStream >> num) {
         FirstVector.push_back(num);
     }
     LineStream.clear();
     getline(std::cin, TextLineTwo);
     LineStream.str(TextLineTwo);
-    while (!std::regex_match(TextLineTwo, rx)) {
-        std::cout << "error\n";
-        LineStream.clear();
-        getline(std::cin, TextLineTwo);
-        LineStream.str(TextLineTwo);
-    }
     while (LineStream >> num) {
         SecondVector.push_back(num);
     }
@@ -90,8 +97,7 @@ int main() {
         std::cout << "the vectors are non equivalent";
         return 0;
     }
-     **/
-    /**double EuclideanNum, ManhattanNum, ChebyshevNum, CanberraNum, MinkowskiNum;
+    double EuclideanNum, ManhattanNum, ChebyshevNum, CanberraNum, MinkowskiNum;
     EuclideanNum = Euclidean(FirstVector, SecondVector);
     ManhattanNum = Manhattan(FirstVector, SecondVector);
     ChebyshevNum = Chebyshev(FirstVector, SecondVector);
@@ -108,7 +114,7 @@ int main() {
     std::cout << CanberraNum << std::endl;
     std::cout.precision(PrecisionNum(MinkowskiNum));
     std::cout << MinkowskiNum << std::endl;
-    return 0;**/
+    return 0;
 
 }
 
